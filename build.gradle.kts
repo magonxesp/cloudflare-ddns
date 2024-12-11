6d70bf0bb8e42026d343adc2d5083fcc94b34a7b
MagonxESP <janma.360@gmail.com>
2024-12-11 15:26:58 +0100

    🚀 bumped to version v0.3.7

--- build.gradle.kts
+++ build.gradle.kts
@@ -9,7 +9,7 @@ plugins {
 }
 
 group = "io.github.magonxesp"
-version = project.properties["gitTag"] ?: "0.2.1"
+version = project.properties["gitTag"] ?: "0.3.7"
 
 repositories {
     mavenCentral()
