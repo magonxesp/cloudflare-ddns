1412ee227634916097063de71d17fc17f560df4a
MagonxESP <janma.360@gmail.com>
2024-12-16 16:10:47 +0100

    chore: update install-debian-amd64.sh

--- scripts/install-debian-amd64.sh
+++ scripts/install-debian-amd64.sh
@@ -2,7 +2,7 @@
 
 wget \
 	"https://github.com/magonxesp/cloudflare-ddns/releases/download/v0.4.0/cloudflare-ddns_0.4.0_amd64.deb" \
-	-O /tmp/cloudflare-ddns.deb
+	-O /tmp/cloudflare-ddns.deb > /dev/null
 
 dpkg -i /tmp/cloudflare-ddns.deb
 rm -rf /tmp/cloudflare-ddns.deb
