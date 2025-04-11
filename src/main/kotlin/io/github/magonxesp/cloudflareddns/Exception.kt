067874b04e39cd1990b238dc397de5295194287f
MagonxESP <janma.360@gmail.com>
2025-04-11 16:36:25 +0200

    ci: push changelog.md changes

--- src/main/kotlin/io/github/magonxesp/cloudflareddns/Exception.kt
+++
@@ -1,8 +0,0 @@
-package io.github.magonxesp.cloudflareddns
-
-fun <T> runCatchingSafe(block: () -> T): Result<T> =
-	try {
-		Result.success(block())
-	} catch (exception: Exception) {
-		Result.failure(exception)
-	}
